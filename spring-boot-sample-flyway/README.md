29853e4508bdf45747754ab470277d4f0152655b
SeokYoung Kim <deuxksy@gmail.com>
2018-11-19 00:41:07 +0900

    주석 추가

--- spring-boot-sample-flyway/README.md
+++ spring-boot-sample-flyway/README.md
@@ -1,4 +1,5 @@
 # Spring Boot Flyway Sample
+DB 를 설치 하지 않고 jvm 용 DB 사용해서 DB 테스트
 
 - Spring Boot Admin  
     http://localhost:8080/actuator/flyway
--- spring-boot-sample-flyway/src/main/resources/db/migration/V2__insert.sql
+++ spring-boot-sample-flyway/src/main/resources/db/migration/V2__insert.sql
@@ -1,2 +1,4 @@
 insert into PERSON (first_name, last_name) values ('석영', '김');
 insert into PERSON (first_name, last_name) values ('장식', '김');
+insert into PERSON (first_name, last_name) values ('미선', '김');
+insert into PERSON (first_name, last_name) values ('솔희', '이');
