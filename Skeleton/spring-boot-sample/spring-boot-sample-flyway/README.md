79a1ccf59c55a02513845a12169a8eddf03a17c8
SeokYoung. Kim <crom@zzizily.com>
2018-12-11 06:21:20 +0900

    Repo 분리에 따른 정리

--- Skeleton/spring-boot-sample/spring-boot-sample-flyway/README.md
+++
@@ -1,9 +0,0 @@
-# Spring Boot Flyway Sample
-DB 를 설치 하지 않고 jvm 용 DB 사용해서 DB 테스트
-
-- Spring Boot Admin  
-    http://localhost:8080/actuator/flyway
-- H2 Web Console  
-    http://localhost:8080/h2-console
-- JDBC URL  
-    jdbc:h2:mem:testdb
--- Skeleton/spring-boot-sample/spring-boot-sample-flyway/src/main/resources/db/migration/V2__insert.sql
+++
@@ -1,4 +0,0 @@
-insert into PERSON (first_name, last_name) values ('석영', '김');
-insert into PERSON (first_name, last_name) values ('장식', '김');
-insert into PERSON (first_name, last_name) values ('미선', '김');
-insert into PERSON (first_name, last_name) values ('솔희', '이');
